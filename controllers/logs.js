07c549ba87d05890825e38921d0d61473e962112
PlatenPrime <proshta@gmail.com>
2023-09-06 19:43:42 +0300

    Update logs.js

--- controllers/logs.js
+++ controllers/logs.js
@@ -31,7 +31,7 @@ export async function createLog(req, res) {
 // Get All Logs
 export async function getAllLogs(req, res) {
 	try {
-		const logs = await Log.find();
+		const logs = await Log.find().sort({ timestamp: -1 });
 		res.status(200).json(logs);
 	} catch (error) {
 		res.status(400).json({ error: 'Failed to get logs' });
