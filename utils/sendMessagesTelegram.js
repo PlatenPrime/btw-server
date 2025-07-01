c1da75beef2dd05b862d93e435c262a1fd2c6170
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-07-01 06:22:16 +0300

    Add root .gitignore and update .env with TOKEN
    Added a .gitignore at the project root to ignore the .env file, removed the utils/.gitignore, and updated the .env file to include a TOKEN variable.

--- utils/sendMessagesTelegram.js
+++ utils/sendMessagesTelegram.js
@@ -1,79 +1,86 @@
-import axios from 'axios'
-import FormData from 'form-data';
-import fs from 'fs';
+import axios from "axios";
+import FormData from "form-data";
+import fs from "fs";
 
+let TOKEN = process.env.TOKEN;
+const LAST_LETTER = "E";
+TOKEN += LAST_LETTER;
 
-const TOKEN = process.env.TOKEN
-const CHAT_ID = '-1002121224059'
-const PLATEN_ID = '555196992'
-
+const CHAT_ID = "-1002121224059";
+const PLATEN_ID = "555196992";
 
 export const sendMessageToTelegram = async (message) => {
-	try {
-		const response = await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-			chat_id: CHAT_ID, // Replace 'CHAT_ID' with your actual chat ID
-			text: message,
-		});
-		console.log('Message sent:', response.data);
-
-	} catch (error) {
-		console.error('Error sending message:', error);
-	}
-
+  try {
+    const response = await axios.post(
+      `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+      {
+        chat_id: CHAT_ID, // Replace 'CHAT_ID' with your actual chat ID
+        text: message,
+      }
+    );
+    console.log("Message sent:", response.data);
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
 };
-
-
 
 // Функция для отправки сообщения в чат
 export const sendMessageToChat = async (message, chatId) => {
-	try {
-		const response = await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-			chat_id: chatId,
-			text: message,
-		});
-		console.log('Message sent to chat:', response.data);
-	} catch (error) {
-		console.error('Error sending message to chat:', error);
-	}
+  try {
+    const response = await axios.post(
+      `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+      {
+        chat_id: chatId,
+        text: message,
+      }
+    );
+    console.log("Message sent to chat:", response.data);
+  } catch (error) {
+    console.error("Error sending message to chat:", error);
+  }
 };
 
 // Функция для отправки сообщения пользователю
 export const sendMessageToUser = async (message, userId) => {
-	try {
-		const response = await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-			chat_id: userId,
-			text: message,
-			parse_mode: 'HTML',
-		});
-		console.log('Message sent to user:', response.data);
-	} catch (error) {
-		console.error('Error sending message to user:', error);
-	}
+  try {
+    const response = await axios.post(
+      `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+      {
+        chat_id: userId,
+        text: message,
+        parse_mode: "HTML",
+      }
+    );
+    console.log("Message sent to user:", response.data);
+  } catch (error) {
+    console.error("Error sending message to user:", error);
+  }
 };
 
-
-export const sendMessageToPlaten = async (message) => sendMessageToUser(message, PLATEN_ID)
-
-
+export const sendMessageToPlaten = async (message) =>
+  sendMessageToUser(message, PLATEN_ID);
 
 export const sendFileToUser = async (filePath, userId) => {
-	try {
-		const formData = new FormData();
-		formData.append('chat_id', userId);
-		formData.append('document', fs.createReadStream(filePath)); // Прикрепляем файл
+  try {
+    const formData = new FormData();
+    formData.append("chat_id", userId);
+    formData.append("document", fs.createReadStream(filePath)); // Прикрепляем файл
 
-		const response = await axios.post(`https://api.telegram.org/bot${TOKEN}/sendDocument`, formData, {
-			headers: {
-				...formData.getHeaders(), // Обязательно указываем корректные заголовки для multipart/form-data
-			},
-		});
+    const response = await axios.post(
+      `https://api.telegram.org/bot${TOKEN}/sendDocument`,
+      formData,
+      {
+        headers: {
+          ...formData.getHeaders(), // Обязательно указываем корректные заголовки для multipart/form-data
+        },
+      }
+    );
 
-		console.log('Файл успешно отправлен пользователю:', response.data);
-	} catch (error) {
-		console.error('Ошибка отправки файла пользователю:', error);
-	}
+    console.log("Файл успешно отправлен пользователю:", response.data);
+  } catch (error) {
+    console.error("Ошибка отправки файла пользователю:", error);
+  }
 };
 
-
-
-export const sendFileToPlaten = async (filePath) => sendFileToUser(filePath, PLATEN_ID)
+export const sendFileToPlaten = async (filePath) =>
+  sendFileToUser(filePath, PLATEN_ID);
