4110b9c3b3093127060a317a961271f2b68df6c8
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-09-30 08:34:44 +0300

    Update index.js

--- utils/cron/index.js
+++ utils/cron/index.js
@@ -1,16 +1,16 @@
 import cron from "node-cron";
 
-import { calculateDefs } from "../defs/calculateDefs.js";
+// import { calculateDefs } from "../defs/calculateDefs.js";
 import { updateAllArtDataComps } from "../comps/updateAllArtDataComps.js";
 import { updateAllArtDataCompVariants } from "../comps/updateAllArtDataCompVariants.js";
 import { sendCollectionsDataToTelegram } from "../reserve/sendCollectionsDataToTelegram.js";
 
 export const cronTasks = () => {
-  cron.schedule("0 6-14 * * 1-5", async () => {
-    console.log("Calculating defs...");
-    await calculateDefs();
-    console.log("Calculating defs finished...");
-  });
+  // cron.schedule("0 6-14 * * 1-5", async () => {
+  //   console.log("Calculating defs...");
+  //   await calculateDefs();
+  //   console.log("Calculating defs finished...");
+  // });
 
   cron.schedule("0 2 * * *", async () => {
     console.log("Updating all comps...");
